571e0805601b222125c052d866bb858328e01ef5
Tulio Cesar Martins Pereira <funroll.loops@gmail.com>
2019-05-13 07:34:36 -0300

    Fix query

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from computer_events.models import ComputerEvents
 
 def index(request):
-    events = ComputerEvents.objects.distinct('computer_id')
+    events = ComputerEvents.objects.distinct('computer_id').order_by('-computer_id','-created_at')
 
     return render(request, 'dashboard/index.html', context={'events': events})
